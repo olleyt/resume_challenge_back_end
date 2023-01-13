dc760cc4aa78e7fce1c71c9ac3ccab876564458a
Olley <goodnewsolley@gmail.com>
2023-01-13 18:35:07 +1100

    refactors update_visitor_counter lambda fucntion

--- update_visitor_counter_lambda.py
+++ update_visitor_counter_lambda.py
@@ -3,6 +3,24 @@ import json
 
 dynamodb = boto3.resource('dynamodb')
 
+def get_item_attr(table_name, partition_key, item_attribute):
+    """
+        gets value of a specific attribute in a DynamoDB table by partition key
+    """
+    try:
+        response = table_name.get_item(
+            Key={ partition_key: item_attribute}
+            )
+    except ClientError as err:
+            logger.error(
+                "Couldn't get visitor counter from table %s. Here's why: %s: %s",
+                self.table.name,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+    else:        
+        item = response['Item']
+    
+    return item[item_attribute]
 
 def lambda_handler(event, context):
     table = dynamodb.Table('visitor_counter')
@@ -15,36 +33,30 @@ def lambda_handler(event, context):
     #)
     
     # get the total visitor count
+    
+    total_visitor_count = get_item_attr(table, 'visitor_count', 'total_count') #item['total_count']
+    print(f'Total visitor count before update {total_visitor_count}')
+    
+    incremented_visitor_count = total_visitor_count + 1
+
     try:
-        response = table.get_item(
-            Key={ 'visitor_count': 'total_count'}
-            )
+        update_response = table.update_item(
+            Key={'visitor_count': 'total_count'},
+            UpdateExpression='SET total_count = :val1',
+            ExpressionAttributeValues={':val1': incremented_visitor_count}
+            )   
     except ClientError as err:
             logger.error(
-                "Couldn't get visitor counter from table %s. Here's why: %s: %s",
+                "Couldn't update visitor counter in the table %s. Here's why: %s: %s",
                 self.table.name,
                 err.response['Error']['Code'], err.response['Error']['Message'])
             raise
     else:        
-        item = response['Item']
+        status_code = update_response['ResponseMetadata']['HTTPStatusCode']
     
-    total_visitor_count = item['total_count']
-    print(f'Total visitor count before update {total_visitor_count}')
-    
-    incremented_visitor_count = total_visitor_count + 1
-    
-    table.update_item(
-    Key={'visitor_count': 'total_count'},
-    UpdateExpression='SET total_count = :val1',
-    ExpressionAttributeValues={
-        ':val1': incremented_visitor_count
-        }
-    )
-    
-    status_code = response['ResponseMetadata']['HTTPStatusCode']
-    
+    updated_total_visitor_count = get_item_attr(table, 'visitor_count', 'total_count')
     message = {
-        'message': f'Total visitor count: {incremented_visitor_count}'
+        'message': f'Total visitor count: {updated_total_visitor_count}'
         }
         
     return {
