82bf5a185903f0586ac1217654238fc82edb4877
Olley <goodnewsolley@gmail.com>
2023-01-14 14:51:54 +1100

    makes lambda running update in separate function

--- update_visitor_counter_lambda.py
+++ update_visitor_counter_lambda.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+from botocore.exceptions import ClientError
 
 dynamodb = boto3.resource('dynamodb')
 
@@ -22,28 +23,18 @@ def get_item_attr(table_name, partition_key, item_attribute):
     
     return item[item_attribute]
 
-def lambda_handler(event, context):
-    table = dynamodb.Table('visitor_counter')
 
-    # only for 1st time
-    #response = table.put_item(
-    #    Item={
-    #        'visitor_count': 0
-    #    }
-    #)
+def update_item (table_name, partition_key, item_attribute, new_value):
+    """
+    Updates item in DynamoDB table by partition key and attribute
+    """
+    status_code = 0
     
-    # get the total visitor count
-    
-    total_visitor_count = get_item_attr(table, 'visitor_count', 'total_count') #item['total_count']
-    print(f'Total visitor count before update {total_visitor_count}')
-    
-    incremented_visitor_count = total_visitor_count + 1
-
     try:
-        update_response = table.update_item(
-            Key={'visitor_count': 'total_count'},
-            UpdateExpression='SET total_count = :val1',
-            ExpressionAttributeValues={':val1': incremented_visitor_count}
+        update_response = table_name.update_item(
+            Key={ partition_key: item_attribute},
+            UpdateExpression='SET ' + item_attribute +' = :val1',
+            ExpressionAttributeValues={':val1': new_value}
             )   
     except ClientError as err:
             logger.error(
@@ -53,6 +44,18 @@ def lambda_handler(event, context):
             raise
     else:        
         status_code = update_response['ResponseMetadata']['HTTPStatusCode']
+    return status_code
+        
+
+def lambda_handler(event, context):
+    table = dynamodb.Table('visitor_counter')
+
+    # get the total visitor count    
+    total_visitor_count = get_item_attr(table, 'visitor_count', 'total_count') #item['total_count']
+    print(f'Total visitor count before update {total_visitor_count}')
+    
+    incremented_visitor_count = total_visitor_count + 1     
+    status_code = update_item(table, 'visitor_count', 'total_count', incremented_visitor_count)
     
     updated_total_visitor_count = get_item_attr(table, 'visitor_count', 'total_count')
     message = {
