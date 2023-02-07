bdbf89a7a99c0fdc55a1aa187f83f20e9a3bcbd9
Olley <goodnewsolley@gmail.com>
2023-02-07 20:30:21 +1100

    adds py script to put initial record to counter dynamoDB table

---
+++ initialise_visitor_counter.py
@@ -0,0 +1,48 @@
+import boto3
+import json
+import logging
+import os
+from botocore.exceptions import ClientError
+
+# enable logging
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR)
+
+
+def init_counter(table_name, partition_key, item_attribute):
+    """
+        gets value of a counter attribute in the DynamoDB table by partition key
+        if the item doesn't exist, it creates it with the counter attribute set to 0
+    """
+    try:
+        response = table_name.get_item(
+            Key={ partition_key: item_attribute}
+            )
+    except ClientError as err:
+        if err.response['Error']['Code'] == 'ValidationException':
+            raise KeyError("Item not found")
+        else:
+            logger.error(
+                "Couldn't get visitor counter from table %s. Here's why: %s: %s",
+                table_name,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+    else:        
+        if 'Item' not in response:
+            table_name.put_item(
+                Item={
+                    partition_key: item_attribute,
+                    item_attribute: 0
+                    })
+        item = response['Item']
+    
+    return item[item_attribute]
+
+if __name__ == '__main__':
+    # initialise DynamoDB resource
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    # get the DynamoDB table
+    table_name = os.environ.get('TABLE_NAME')
+    table = dynamodb.Table(table_name)
+    # initialise the counter
+    init_counter(table, 'visitor_count', 'total_count')
